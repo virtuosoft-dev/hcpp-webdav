2a8394b64c8dbba0a44a812a0330775b2ac8abc5
Stephen J. Carnam <steve@steveorevo.com>
2023-06-05 01:52:28 -0700

    wip

--- webdav.php
+++ webdav.php
@@ -12,37 +12,108 @@
 
 if ( ! class_exists( 'WebDAV') ) {
     class WebDAV {
-    
+        
         /**
          * Constructor, listen for add, update, or remove users
          */
         public function __construct() {
             global $hcpp;
             $hcpp->webdav = $this;
-            $hcpp->add_action( 'priv_add_user', [ $this, 'priv_add_user'] );
-            $hcpp->add_action( 'priv_delete_user', [ $this, 'priv_delete_user'] );
-            $hcpp->add_action( 'priv_unsuspend_domain', [ $this, 'priv_unsuspend_domain'] );
+            $hcpp->add_action( 'priv_unsuspend_domain', [ $this, 'priv_unsuspend_domain' ] );
+            $hcpp->add_action( 'new_web_domain_ready', [ $this, 'new_web_domain_ready' ] );
+            $hcpp->add_action( 'priv_delete_user', [ $this, 'priv_delete_user' ] );
         }
         
-        /**
-         *
-         */
-        public function priv_add_user() {
-            
+        // Trigger setup when domain is created.
+        public function new_web_domain_ready( $args ) {
+            global $hcpp;
+            $user = $args[0];
+            $this->setup( $user );           
+            return $args;
+        }
+
+        // On domain unsuspend, re-run setup
+        public function priv_unsuspend_domain( $args ) {
+            global $hcpp;
+            $user = $args[0];
+            $this->setup( $user );           
+            return $args;
         }
         
-        /**
-         *
-         */
-        public function priv_delete_user() {
-            
+        // Setup WebDAV services for users
+        public function setup( $user ) {
+            global $hcpp;
+            $hostname = trim( $hcpp->delLeftMost( shell_exec( 'hostname -f' ), '.' ) );
+            $conf = "/home/$user/conf/web/webdav-$user.$hostname/nginx.conf";
+
+            // Create the configuration folder
+            if ( ! is_dir( "/home/$user/conf/web/webdav-$user.$hostname" ) ) {
+                mkdir( "/home/$user/conf/web/webdav-$user.$hostname" );
+            }
+
+            // Get user account first IP address.
+            $ip = array_key_first(
+                json_decode( shell_exec( '/usr/local/hestia/bin/v-list-user-ips ' . $user . ' json' ), true ) 
+            );
+
+            // Create the nginx.conf file.
+            $content = file_get_contents( __DIR__ . '/conf-web/nginx.conf' );
+            $content = str_replace( 
+                ['%ip%', '%user%', '%hostname%'],
+                [$ip, $user, $hostname],
+                $content
+            );
+            file_put_contents( $conf, $content );
+
+// TODO: WIP
+
+
+            // Create the nginx.conf_nodeapp file.
+            $conf = "/home/$user/conf/web/webdav-$user.$hostname/nginx.conf_nodeapp";
+            $content = file_get_contents( __DIR__ . '/conf-web/nginx.conf_nodeapp' );
+            file_put_contents( $conf, $content );
+
+            // Allocate a port for the VSCode Server instance.
+            $hcpp->allocate_port( "vscode_port", $user, "vscode-$user.$hostname" );
+
+            // Create the nginx.forcessl.conf_ports file.
+            $conf = "/home/$user/conf/web/vscode-$user.$hostname/nginx.forcessl.conf_ports";
+            $content = file_get_contents( __DIR__ . '/conf-web/nginx.forcessl.conf_ports' );
+            $content = str_replace( 
+                ['%user%', '%hostname%'],
+                [$user, $hostname],
+                $content
+            );
+            file_put_contents( $conf, $content );
+
+            // Create the NGINX configuration symbolic link.
+            $link = "/etc/nginx/conf.d/domains/vscode-$user.$hostname.conf";
+            if ( ! is_link( $link ) ) {
+                symlink( "/home/$user/conf/web/vscode-$user.$hostname/nginx.conf", $link );
+            }
+
+            // Start the VSCode Server instance
+            $cmd = "runuser -l $user -c \"cd /opt/vscode && source /opt/nvm/nvm.sh ; pm2 pid vscode-$user.$hostname\"";
+            if ( trim( shell_exec( $cmd ) ) === '' ) {
+                $cmd = "runuser -l $user -c \"cd /opt/vscode && source /opt/nvm/nvm.sh ; pm2 start vscode.config.js\"";
+                $hcpp->log( shell_exec( $cmd ) );
+            }else{
+                $this->update_token( $user );
+            }
+        }
+
+
+        // Delete the NGINX configuration reference and server when the user is deleted.
+        public function priv_delete_user( $args ) {
+            global $hcpp;
+            $user = $args[0];
+            $hostname = trim( $hcpp->delLeftMost( shell_exec( 'hostname -f' ), '.' ) );
+            $link = "/etc/nginx/conf.d/domains/vscode-$user.$hostname.conf";
+            if ( is_link( $link ) ) {
+                unlink( $link );
+            }
+            return $args;
         }
         
-        /**
-         *
-         */
-        public function priv_unsuspend_domain() {
-            
-        } 
     }
 }
